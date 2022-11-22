a2b096a166900abf2127c1434d9d03399e9271ca
BohdanSivak <BohdanSivak@AT-622550>
2022-11-22 16:53:33 +0100

    Added Linq2db context

--- src/playme_api/Program.cs
+++ src/playme_api/Program.cs
@@ -1,22 +1,39 @@
+using LinqToDB.AspNet.Logging;
+using LinqToDB.Common;
+using LinqToDB.Configuration;
+using LinqToDB.AspNet;
 using playme_api.Helper;
+using LinqToDB;
+using System;
+using playme_api.Models;
+using playme_api.Interfaces;
+using playme_api.Models.DAL;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Controllers
-builder.Services.AddSingleton<DapperContext>();
+//builder.Services.AddSingleton<DapperContext>();
 builder.Services.AddHealthChecks();
 
 // Add DALs
 //builder.Services.AddScoped<IUsersRepository, UsersRepository>();
-
+builder.Services.AddScoped<IUsersRepository, UsersRepository>();
 // Add services to the container.
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(config =>
 {
     //use fully qualified object names
     config.CustomSchemaIds(x => x.FullName);
+});
+
+
+//Linq2Db Connection
+builder.Services.AddLinqToDBContext<AppDataConnection>((provider, options) => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")).UseDefaultLogging(provider);
 });
 
 var app = builder.Build();
